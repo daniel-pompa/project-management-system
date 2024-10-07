6eb63cc3de42656f52ff7c350be0131dd3ad47de
Daniel <daniel.40.pompa@gmail.com>
2024-10-07 23:54:48 +0200

    feat: add delete note functionality
    - Implemented `deleteNote` API call for deleting notes.
    - Enhanced `NoteDetail` component to include delete button.
    - Added user permission check to enable delete action.
    - Integrated toast notifications for successful deletion.
    - Implemented loading state handling while fetching user data.

--- src/api/note-api.ts
+++ src/api/note-api.ts
@@ -1,11 +1,12 @@
 import api from '@/lib/axios';
-import { NoteFormData, Project, Task } from '@/types';
+import { Note, NoteFormData, Project, Task } from '@/types';
 import { isAxiosError } from 'axios';
 
 type NoteAPIType = {
   formData: NoteFormData;
   projectId: Project['_id'];
   taskId: Task['_id'];
+  noteId: Note['_id'];
 };
 
 export const createNote = async ({
@@ -16,6 +17,22 @@ export const createNote = async ({
   try {
     const url = `/projects/${projectId}/tasks/${taskId}/notes`;
     const { data } = await api.post(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response?.data?.message);
+    }
+  }
+};
+
+export const deleteNote = async ({
+  projectId,
+  taskId,
+  noteId,
+}: Pick<NoteAPIType, 'projectId' | 'taskId' | 'noteId'>) => {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`;
+    const { data } = await api.delete(url);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,18 +1,61 @@
+import { useMemo } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import { FaTrashAlt } from 'react-icons/fa';
 import { Note } from '@/types';
+import { deleteNote } from '@/api';
 import { formatDate } from '@/utils/utils';
+import { useAuth } from '@/hooks/useAuth';
+import { Spinner } from '../Spinner/Spinner';
 
 type NoteDetailProps = {
   note: Note;
 };
 
 export const NoteDetail = ({ note }: NoteDetailProps) => {
+  const { data, isLoading } = useAuth();
+
+  const canUserDeleteNote = useMemo(() => data?._id === note.createdBy._id, [data, note]);
+
+  const params = useParams();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const projectId = params.projectId!;
+  const taskId = queryParams.get('view-task')!;
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onSuccess: data => {
+      toast.success(data.message);
+      queryClient.invalidateQueries({ queryKey: ['task', taskId] });
+    },
+    onError: error => {
+      toast.error(error.message);
+    },
+  });
+
+  if (isLoading) return <Spinner />;
+
   return (
-    <div className='flex justify-between items-center p-2'>
+    <div className='flex justify-between items-center p-2 gap-2'>
       <div className='space-y-1'>
         <p className='font-bold'>Creada por {note.createdBy.name}</p>
         <p className='text-slate-600'>{note.content}</p>
         <p className='text-sm text-slate-500'>{formatDate(note.createdAt)}</p>
       </div>
+      {canUserDeleteNote && (
+        <button
+          type='button'
+          className='text-red-500 hover:text-red-600 transition-colors'
+          onClick={() => mutate({ projectId, taskId, noteId: note._id })}
+        >
+          <FaTrashAlt />
+        </button>
+      )}
     </div>
   );
 };
