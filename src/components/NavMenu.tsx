e40d9ff175d0048b1d23da5bba7a0e9ad941daa1
Daniel <daniel.40.pompa@gmail.com>
2025-09-14 23:57:36 +0200

    style: update nav menu color scheme and spacing

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -17,24 +17,27 @@ export const NavMenu = ({ name }: NavMenuProps) => {
   };
 
   return (
-    <div className='flex justify-center md:justify-between mt-5 md:mt-0 w-full'>
+    <div className='flex justify-center md:justify-between w-full'>
       <Menu>
         <div className='flex space-x-4'>
           <MenuItem>
-            <Link to='/profile' className='block md:text-xl hover:text-cyan-400'>
+            <Link
+              to='/profile'
+              className='block md:text-xl text-slate-100 hover:text-slate-400'
+            >
               Perfil
             </Link>
           </MenuItem>
           <MenuItem>
-            <Link to='/' className='block md:text-xl hover:text-cyan-400'>
+            <Link to='/' className='block md:text-xl text-slate-100 hover:text-slate-400'>
               Proyectos
             </Link>
           </MenuItem>
         </div>
         <div className='flex space-x-3 ms-4 md:ms-0'>
-          <p className='md:text-xl'>Hola, {name}</p>
-          <MenuButton className='hover:text-cyan-400'>
-            <AiOutlineLogout className='h-5 w-5 md:w-6 md:h-6' onClick={logout} />
+          <p className='md:text-xl text-slate-100'>Hola, {name}</p>
+          <MenuButton className='text-slate-100 hover:text-slate-400'>
+            <AiOutlineLogout className='h-4 w-4 md:w-6 md:h-6' onClick={logout} />
           </MenuButton>
         </div>
       </Menu>
